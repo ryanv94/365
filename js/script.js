01f14abcbd7bc5b7c8e9fa0dcd73d5a3f9dc182b
Ryan Veitch <37779995+ryanv94@users.noreply.github.com>
2021-04-27 16:00:39 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 var width = window.screen.width;
 var height = window.screen.height;
 
+var concatRes = width.concat('x', height);
+
 if (typeof newrelic == 'object') {
 newrelic.setCustomAttribute('screenWidth', width);
 newrelic.setCustomAttribute('screenHeight', height);
+newrelic.setCustomAttribute('screenResolution', concatRes);
 }
 
