08db93f856be185ca43b202cf457bd699ab0e1b9
Ryan Veitch <37779995+ryanv94@users.noreply.github.com>
2021-04-29 09:48:32 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var width = window.screen.width;
 var height = window.screen.height;
 
+//Convert to string for concatenation
 var strWidth = width.toString();
 var strHeight = height.toString();
-
 var concatRes = strWidth.concat('x', strHeight);
 
 if (typeof newrelic == 'object') {
